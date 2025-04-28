85fbbabd2f7f220d50285f4227bfc7edf1eb0af8
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-04-28 10:43:22 +0530

    refactor: Clean up authMiddleware comments and enhance tasksRoutes import structure

--- backend/src/routes/tasksRoutes.js
+++ backend/src/routes/tasksRoutes.js
@@ -1,4 +1,4 @@
-import { createTask } from '../controllers/task/taskController.js';
+import { createTask, getTasks } from '../controllers/task/taskController.js';
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
